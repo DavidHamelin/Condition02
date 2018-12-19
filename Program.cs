56eaa9a7493af06a835e63aaf21225b0cf800015
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-19 10:25:16 +0100

    ex 02 conditions

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condition02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour, saisissez votre age :");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 18) Console.WriteLine($"Vous avez {age} ans, vous êtes donc majeur !");
+            else Console.WriteLine($"Vous avez {age} ans, vous êtes donc mineur !");
+        }
+    }
+}

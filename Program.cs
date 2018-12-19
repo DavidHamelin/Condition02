fcaed15254cab449fbd664b6baebe443718e38a2
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-19 13:52:33 +0100

    correction

--- Program.cs
+++ Program.cs
@@ -13,8 +13,18 @@ namespace Condition02
             Console.WriteLine("Bonjour, saisissez votre age :");
             int age = int.Parse(Console.ReadLine());
 
-            if (age >= 18) Console.WriteLine($"Vous avez {age} ans, vous êtes donc majeur !");
-            else Console.WriteLine($"Vous avez {age} ans, vous êtes donc mineur !");
+            if (age >= 18 && age < 120)
+            {
+                Console.WriteLine($"Vous avez {age} ans, vous êtes donc majeur !");
+            }
+            else if (age < 18 && age > 0)
+            {
+                Console.WriteLine($"Vous avez {age} ans, vous êtes donc mineur !");
+            }
+            else 
+            {
+                Console.WriteLine("Oops, il doit y avoir une erreur !");
+            }
         }
     }
 }
